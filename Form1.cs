9c4d39cca6f1cc3122d0a5f038bfebd5d1806300
JakubRoss <Jakub.Rosploch@gmail.com>
2023-07-15 00:00:02 +0200

    KungFu

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ namespace simpleCalculator
     {
         private bool AddSubFlag = true;
         private string KungFu = "༼ つ ◕◡◕ ༽つ";
+        private bool KungFuFlag = false;
         public mainWindow()
         {
             InitializeComponent();
@@ -47,9 +48,12 @@ namespace simpleCalculator
             Expression expression = new Expression(string.Join("", newEquation));
             return expression.calculate();
         }
+
         private void buttonEqual_Click(object sender, EventArgs e)
         {
-            var lastChar = textBox.Text.LastOrDefault();
+            if (KungFuFlag)
+                return;
+                var lastChar = textBox.Text.LastOrDefault();
             if (lastChar != 247 && lastChar != 120 && lastChar != default(char))
             {
                 var result = calculations(textBox.Text);
@@ -57,18 +61,23 @@ namespace simpleCalculator
                 textBox.Text = resultTxt;
             }
         }
-        private void buttonBackspace_Click(object sender, EventArgs e)
+        private void buttonBackspace_Click(object sender, EventArgs e)//
         {
-            if (textBox.Text.Length > 0 && textBox.Text !=KungFu)
+            if (textBox.Text.Length > 0 && textBox.Text != KungFu)
             {
                 textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1, 1);
             }
             else if (textBox.Text.Contains(KungFu))
+            {
                 textBox.Text = string.Empty;
+                KungFuFlag = false;
+            }
             
         }
         private void buttonAddSub_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             var lastChar = textBox.Text.LastOrDefault();
             if (AddSubFlag && lastChar != default(char))
             {
@@ -85,78 +94,110 @@ namespace simpleCalculator
                 AddSubFlag = true;
             }
         }
-        private void buttonClear_Click(object sender, EventArgs e)
+        private void buttonClear_Click(object sender, EventArgs e)//
         {
             textBox.Text = string.Empty;
+            KungFuFlag = false;
         }
         private void buttonKungFu_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text = KungFu;
+            KungFuFlag = true;
         }
 
         #region chars
         private void button1_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "1";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "2";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "3";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "4";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "5";
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "6";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "7";
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "8";
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "9";
         }
         private void buttonComa_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += ",";
         }
         private void buttonLeftParenthesis_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "(";
         }
         private void buttonRightParenthesis_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag || textBox.Text.Length <1)
+                return;
             textBox.Text += ")";
         }
         private void buttonMultiply_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "x";
         }
         private void buttonDivine_Click(object sender, EventArgs e)
         {
+            if (KungFuFlag)
+                return;
             textBox.Text += "÷";
         }
         #endregion
